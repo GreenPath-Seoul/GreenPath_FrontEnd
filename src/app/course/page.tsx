4333e122c5354859ccc278bb59bdff62d77db343
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-04-03 22:09:05 +0900

    feat: 코스 목록 페이지 수정

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -90,7 +90,7 @@ export default function CourseListView() {
         {/* 리스트 */}
         <div
           style={{
-            padding: "0 20px",
+            padding: "0 20px 100px 20px",
             display: "flex",
             flexDirection: "column",
             gap: "16px"
